0608cf9d7589429334561af75166e2f2116fc36a
happylovehometan <happylovehometan@gmail.com>
2026-06-16 20:00:47 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "happy-love-home-v1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
